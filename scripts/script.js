5dc2d2a8c64ae00695cf80eb16817a46a533d88f
Sarthak-is-here <sarthak6410@gmail.com>
2025-08-15 16:01:09 +0530

    Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,118 @@
-let qqw = 2;
-
+const loadBtn = document.querySelector("#load");
+const input = document.querySelector("#input");
 const msg = document.querySelector(".messageBox");
 const score = document.querySelector("#score");
+const btn = document.querySelector("#btn");
+const attempt = document.querySelector("#attempt");
+const attempts = document.querySelector("#attempts");
 
-document.querySelector("#box").style.height = window.innerHeight - 100 + "px" ;
 
+let attLeft;
+let randomNumber = null;
 
-const attempt = document.getElementById("attempt");
+loadBtn.addEventListener("click", playGame);
 
-function play() {
-let min = parseInt(document.querySelector("#min").value);
-let max = parseInt(document.querySelector("#max").value);
-let attLeft =7;
-if (isNaN(min) || isNaN(max))  {
-    console.log("invalid");
-}
-let rn = Math.floor(Math.random() * (max - min)+min);
-console.log(parseInt(rn));
-    let input =  parseInt(document.getElementById("input").value);
-    localStorage.setItem("num" ,rn); 
-    msg.innerText = rn;
+function playGame() {
+    let min = parseInt(document.querySelector("#min").value);
+    let max = parseInt(document.querySelector("#max").value);
 
-    // if(isNaN(input)) {
-    //     alert("Please enter a valid value - 'SARTHAK' .");
-    //     msg.innerText = "Enter a valid value.";
-    //     msg.style.backgroundColor = "red";
-    // }  
-
-    if(input !== rn){
-        if (input > rn) {
-            msg.innerText = "bigger number";
-            attLeft = attLeft-1;
-            attempt.innerText = `Attemps left = ${attLeft}`;
-        } else if (input < rn)  {
-            msg.innerText = "smaller number";
-            attLeft = attLeft-1;
-            attempt.innerText = `Attemps left = ${attLeft}`;
-        }
-    } else{
-        userWin();
+    if (isNaN(min) || isNaN(max) || max <= min) {
+        alert("Please enter valid min and max values (max > min).");
+        return;
+    }
+    let difference = max -min;
+    if (difference <= 5) {
+        attLeft =2;
+    }else if(difference <= 10) {
+        attLeft = 3;
+    }else if(difference <= 50) {
+        attLeft = 5;
+    }else if(difference <= 100) {
+        attLeft = 7;
     }
     
+    
+    
+    attempt.innerText = ` ${attLeft}`;
+
+    randomNumber = Math.floor(Math.random() * (max - min) + min);
+
+    loadGame();
+}
+
+function play() {
+    let userInput = parseInt(document.getElementById("input").value);
+    msg.innerText = randomNumber;
+
+    if (isNaN(userInput)) {
+        alert("Please enter a valid value - 'SARTHAK' .");
+        msg.innerText = "Enter a valid value.";
+        msgRed();
+        return;
+    }
+
+    if (userInput !== randomNumber) {
+        if (userInput > randomNumber) {
+            msgBlue();
+            msg.innerText = "bigger number";
+            attLeft = attLeft - 1;
+            attempt.innerText = ` ${attLeft}`;
+        } else if (userInput < randomNumber) {
+            msgBlue();
+            msg.innerText = "smaller number";
+            attLeft = attLeft - 1;
+            attempt.innerText = ` ${attLeft}`;
+        }
+    } else {
+        userWin();
+        return;
+    }
     if (attLeft === 0) {
         userLose();
     }
+
     function userWin() {
-        msg.innerText = `You won , the number was ${rn}`;
+        msg.innerText = `You won, the number was ${randomNumber}`;
         msg.style.backgroundColor = "green";
         score.innerText = `You scored ${attLeft}`;
-    
     }
-    
+
     function userLose() {
-        msg.innerText = `You lost , the number was ${rn}`;
+        msg.innerText = `You lost, the number was ${randomNumber}`;
         msg.style.backgroundColor = "red";
         score.innerText = `You scored 0`;
-    
     }
+}
+
+btn.addEventListener('click', play);
+
+function loadGame() {
+    input.classList.remove("invisible");
+    score.classList.remove("invisible");
+    btn.classList.remove("invisible");
+    attempt.classList.remove("invisible");
+    attempts.classList.remove("invisible");
+
+
+    loadBtn.classList.add("invisible");
+    document.querySelector("#max").classList.add("invisible");
+    document.querySelector("#min").classList.add("invisible");
+    document.querySelector(".navbar").classList.add("top");
+    document.querySelector(".holder").classList.add("max");
+
 
 }
 
-const btn = document.getElementById("btn");
-btn.addEventListener('click' ,play);
+document.querySelector("#box").style.height = window.innerHeight - 100 + "px";
+document.querySelector("#message").style.height = window.innerHeight - 150 + "px";
+
+function msgRed() {
+    msg.classList.remove("invisible");
+    msg.style.borderColor = "red";
+}
+function msgBlue() {
+    msg.classList.remove("invisible");
+    msg.style.borderColor = "blue";
+    msg.style.backgroundColor = "#0011ff41";
+
+}
